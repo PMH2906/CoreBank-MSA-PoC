5622656e612a7ef1cafe2c959663176d8d3b2e3c
yeomhyeji <yheji0321@gmail.com>
2024-06-17 14:32:55 +0900

    project setting

---
+++ deposit-withdrawal/src/main/java/org/tmax/depositwithdrawal/DepositWithdrawalApplication.java
@@ -0,0 +1,13 @@
+package org.tmax.depositwithdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositWithdrawalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepositWithdrawalApplication.class, args);
+    }
+
+}
---
+++ deposit-withdrawal/src/test/java/org/tmax/depositwithdrawal/DepositWithdrawalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tmax.depositwithdrawal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositWithdrawalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
