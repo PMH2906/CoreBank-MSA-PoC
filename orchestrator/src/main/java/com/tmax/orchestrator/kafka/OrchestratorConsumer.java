35e9f66b9738dc83ff9561c5e9b84f33ec0b17d6
Kim Hyunji <hzunhy@gmail.com>
2024-06-25 13:58:37 +0900

    debezium 연결

---
+++ orchestrator/src/main/java/com/tmax/orchestrator/kafka/OrchestratorConsumer.java
@@ -0,0 +1,17 @@
+package com.tmax.orchestrator.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrchestratorConsumer {
+
+    @KafkaListener(topics = "ORCHESTRATOR_test_topic5", groupId = "test-group")
+    public void consume(String message) {
+        log.info(message);
+    }
+}
